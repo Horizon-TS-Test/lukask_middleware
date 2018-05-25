46211823f0267c07041dec0c7c043e8bebab22d2
DARIO VARGAS <dvargas@horizon-ts.com>
2018-05-25 10:04:50 -0500

    Archivos para arrancar la aplicacion

--- src/lukask_middleware/config/rest-api-url.js
+++ src/lukask_middleware/config/rest-api-url.js
@@ -1,5 +1,5 @@
 module.exports = {
-    pub: 'http://192.168.1.57:8081/lukask-api/publication/',
-    quejaType: 'http://192.168.1.57:8081/lukask-api/typePub/',
-    login: 'http://192.168.1.57:8081/lukask-api/login/'
+    pub: 'http://192.168.1.57:8080/lukask-api/publication/',
+    quejaType: 'http://192.168.1.57:8080/lukask-api/typePub/',
+    login: 'http://192.168.1.57:8080/lukask-api/login/'
 }
