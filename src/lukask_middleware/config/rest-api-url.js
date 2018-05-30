7df9791254c63a118897ad10e58d9913bcb3d404
FREDI ROMAN <kunfelt_f@hotmail.com>
2018-05-30 09:36:01 -0500

    Channels API websocket client fixed

--- src/lukask_middleware/config/rest-api-url.js
+++ src/lukask_middleware/config/rest-api-url.js
@@ -1,5 +1,5 @@
 module.exports = {
-    pub: 'http://192.168.1.57:8081/lukask-api/publication/',
-    quejaType: 'http://192.168.1.57:8081/lukask-api/typePub/',
-    login: 'http://192.168.1.57:8081/lukask-api/login/'
+    pub: 'http://192.168.1.56:8081/lukask-api/publication/',
+    quejaType: 'http://192.168.1.56:8081/lukask-api/typePub/',
+    login: 'http://192.168.1.56:8081/lukask-api/login/'
 }
