782f2b4e2680744c7c5f7348f609cdf9d88657dc
DARIO VARGAS <dvargas@horizon-ts.com>
2018-06-15 11:29:58 -0500

    Se agrego la ruta Edición de perfil

--- src/lukask_middleware/app.js
+++ src/lukask_middleware/app.js
@@ -33,6 +33,7 @@ var bodyParser = require('body-parser');
 /**
  * ///////////////////////////ROUTES://///////////////////////
  */
+var userRoute = require('./routes/user');
 var relevanceRoute = require('./routes/relevance');
 var commentRoute = require('./routes/comment');
 var qtypeRoute = require('./routes/qtype');
@@ -276,6 +277,7 @@ io.use(function (socket, next) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+app.use('/user', userRoute);
 app.use('/relevance', relevanceRoute);
 app.use('/comment', commentRoute);
 app.use('/qtype', qtypeRoute);
--- src/lukask_middleware/config/rest-api-url.js
+++ src/lukask_middleware/config/rest-api-url.js
@@ -2,5 +2,6 @@ module.exports = {
     pub: 'http://192.168.1.57:8080/lukask-api/publication/',
     action: 'http://192.168.1.57:8080/lukask-api/actionPub/',
     quejaType: 'http://192.168.1.57:8080/lukask-api/typePub/',
-    login: 'http://192.168.1.57:8080/lukask-api/login/'
+    login: 'http://192.168.1.57:8080/lukask-api/login/',
+    user: 'http://192.168.1.57:8080/lukask-api/userProfile/'
 }
