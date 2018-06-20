370c747285c86a600a0bf4d6f8789d9f7bbda84b
DARIO VARGAS <dvargas@horizon-ts.com>
2018-06-20 17:15:10 -0500

    Factorización de código foto.

---
+++ src/lukask_middleware/rest-client/user-client.js
@@ -0,0 +1,59 @@
+var restUrl = require('./../config/rest-api-url');
+
+var Client = require("node-rest-client").Client;
+////////////////// MULTIPART/FORM-DATA REQUESTS /////////////////////
+
+var request = require('request');
+////////////////////// FILE MANAGER ////////////////////////
+
+var fs = require("fs");
+////////////////////////////////////////////////////////////
+
+var postUser = function (user_id,body, file, token, callback) {
+    ////////////////////////////////// POST REQUEST //////////////////////////////////////
+    var r = request.patch(
+        {
+            url: restUrl.user+user_id+"/",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Token " + token
+            }
+        }, function optionalCallback(err, httpResponse, data) {
+            if (err) {
+                console.log('Error while making todo post request: ', err);
+                callback(httpResponse.statusCode, err);
+            }
+            if (httpResponse.statusCode == 200) {
+                if (file) {
+                    for (var i = 0; i < file.length; i++) {
+                        console.log("Elimando archivo: " + file[i].path);
+                        fs.unlink(file[i].path);
+                    }
+                }
+                console.log('Publication has been created successfully, Server responded with: ', JSON.parse(data));
+                callback(httpResponse.statusCode, JSON.parse(data));
+            }
+            else {
+                console.log('Error while making todo post request: ', data);
+                callback(httpResponse.statusCode, data);
+            }
+        });
+
+    var form = r.form();
+
+    form.append('id', body.id);
+    form.append('email', body.email);
+    form.append('person.age', body.age);
+    form.append('person.identification_card', body.identification_card);
+    form.append('person.name', body.name);
+    form.append('person.last_name', body.last_name);
+    form.append('person.telephone', body.telephone);
+    form.append('person.address', body.address);
+    form.append('is_active', body.is_active);
+    form.append("media_profile", fs.createReadStream(file.path), { filename: file.originalname, contentType: file.mimetype } );
+    //////////////////////////////////////////////////////////////////////////////////////
+}
+
+module.exports = {
+    postUser: postUser,
+}
---
+++ src/lukask_middleware/routes/user.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+var userRestClient = require('./../rest-client/user-client');
+/////////////////////// FILE UPLOAD ////////////////////////
+var multer = require("multer");
+var upload = multer({ dest: 'tmp_uploads/' });
+
+var wepushClient = require('./../rest-client/webpush-client');
+
+router.post('/:user_id',upload.single('user_file'), function (req, res, next) {
+  let token = req.session.key.token;
+  let user_id=req.params.user_id;
+
+  userRestClient.postUser(user_id,req.body, req.file, token, function (responseCode, data) {
+    if (responseCode == 200) {
+      wepushClient.notify('Usuario modificado', req.body.detail, '/inicio', function (resCode, notifData) {
+        console.log(resCode, notifData);
+      });
+
+      return res.status(responseCode).json({
+        code: responseCode,
+        title: "Publication has been created successfully",
+        pub: data
+      });
+    }
+    return res.status(responseCode).json({
+      code: responseCode,
+      title: "An error has occurred",
+      error: data
+    });
+  });
+
+});
+
+module.exports = router;
