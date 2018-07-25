da52bd042e663dca2e0671f242eae35c7186e27d
dennysmoyon <dmoyon@horizon-ts.com>
2018-07-25 13:09:09 -0500

    add message de comments

--- src/lukask_middleware/routes/comment.js
+++ src/lukask_middleware/routes/comment.js
@@ -50,29 +50,86 @@ router.post('/', upload.single('media_file'), function (req, res, next) {
   req.body.action_type = commentType;
 
   console.log(req.body);
+
   actionRestClient.postAction(req.body, req.file, token, function (responseCode, data) {
     if (responseCode == 201) {
       let userNotif = data.receivers;
+      let ownerPubName, ownerComName;
+      let ownerPubId, ownerComId;
+
 
       if (userNotif.length > 0) {
+        if (data.pub_owner) {
+          console.log(data.pub_owner);
+          ownerPubName = data.pub_owner.user_name;
+          ownerPubId = data.pub_owner.user_id;
+        }
+        if (data.action_parent_owner) {
+          console.log(data.action_parent_owner);
+          ownerComName = data.action_parent_owner.user_name;
+          ownerComId = data.action_parent_owner.id;
+        }
+
         let receivers = [];
         let userEmitter = req.body.userName;
-        //let emitterId = req.body.userId;
+        let emitterId = req.body.userId;
         let emitterImage = req.body.userImage;
         let title = 'Lukask, expresa tu opinión';
         let content;
         let defaultUrl = '/activity?pubId=' + data.publication + ((data.action_parent) ? '&comId=' + data.action_parent + '&repId=' + data.id_action : '&comId=' + data.id_action);
 
         for (let user of userNotif) {
-          if (user.fields.owner == true) {
-            content = userEmitter + " ha" + ((req.body.action_parent) ? " respondido un comentario de tu publicación" : " comentado tu publicación");
+          if (user.fields.owner_publication == true) {
+            if (req.body.action_parent) {
+              if (user.fields.owner_commet == true) {
+                content = userEmitter + " ha respondido tu comentario de tu publicación";
+              }
+              else if (emitterId == ownerComId) {
+                content = userEmitter + " ha respondido su comentario de tu publicación";
+              }
+              else {
+                content = userEmitter + " ha respondido el comentario de " + ownerComName + " de tu publicación";
+              }
+            }
+            else {
+              content = userEmitter + " ha comentado tu publicación";
+            }
           }
           else {
-            /*if(emitterId == data.pub_owner) {
-              content = userEmitter + " ha" + ((req.body.action_parent) ? " respondido un comentario de su publicación" : " comentado su publicación");
-            }*/
-            content = userEmitter + " ha" + ((req.body.action_parent) ? " respondido un comentario de la publicación que has comentado" : " comentado la publicación en la que has interactuado");
+            if (emitterId == ownerPubId) {
+              if (req.body.action_parent) {
+                if (emitterId == ownerComId) {
+                  content = userEmitter + " también ha respondido su comentario de su publicación";
+                }
+                else if (user.fields.owner_commet == true) {
+                  content = userEmitter + " ha respondido tu comentario de su publicación";
+                }
+                else {
+                  content = userEmitter + " también ha respondido el comentario de " + ownerComName + " de su publicación";
+                }
+              }
+              else {
+                content = userEmitter + " tambén ha comentado su publicación";
+              }
+            }
+            else {
+              if (req.body.action_parent) {
+                if (emitterId == ownerComId) {
+                  content = userEmitter + " también ha respondido su comentario de la publicación de " + ownerPubName;
+                }
+                else if (user.fields.owner_commet == true) {
+                  content = userEmitter + " ha respondido tu comentario de la publicación de " + ownerPubName;
+                }
+                else {
+                  content = userEmitter + " también ha respondido el comentario de " + ownerComName + " de la publicación de " + ownerPubName;
+                }
+              }
+              else {
+                content = userEmitter + " también ha comentado la publicación de " + ownerPubName;
+              }
+            }
           }
+
           receivers[receivers.length] = {
             user_img: emitterImage,
             user_id: user.fields.user_register,
@@ -84,12 +141,18 @@ router.post('/', upload.single('media_file'), function (req, res, next) {
         }
 
         notifRestClient.postNotifications(title, req.body.date, defaultUrl, receivers, token, function (notCode, notData) {
+          console.log("respondiendo desde notifRestCient");
           console.log(notCode, notData);
         });
 
+        console.log("Después de postNotifications");
+
         wepushClient.notify(receivers, function (resCode, notifData) {
+          console.log("respondiendo desde webpushClient");
           console.log(resCode, notifData);
         });
+
+        console.log("Después de webPushClient");
       }
 
       return res.status(responseCode).json({
