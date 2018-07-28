6d6c1e4dcbb2fe43a75503fa377b76d5780d7ca8
dennysmoyon <dmoyon@horizon-ts.com>
2018-07-28 14:11:42 -0500

    Process to remove kurento midd server

--- src/lukask_middleware/config/kurento-server.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    kurentoServer_url_ws : "ws://192.168.1.48:8888/kurento"
-}
--- src/lukask_middleware/config/push-api.js
+++ src/lukask_middleware/config/push-api.js
@@ -1,3 +1,3 @@
 module.exports = {
-    webpush_url: 'http://192.168.1.13:3200/notification'
+    webpush_url: 'http://192.168.1.6:3200/notification'
 }
