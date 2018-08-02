6e325f91997675cf62327c31d17c4465f0f0086d
José Luis Buenaño Carrillo <37373548+JoseBuenano@users.noreply.github.com>
2018-08-01 22:22:56 -0500

    Update servers.js

--- src/lukask_middleware/config/servers.js
+++ src/lukask_middleware/config/servers.js
@@ -1,5 +1,5 @@
 module.exports = {
-    allow_origin: 'http://127.0.0.1:4200',
+    allow_origin: 'http://192.168.1.2:8080',
     //allow_origin: 'http://192.168.1.6:4200',
     backend_websocket: 'ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080',
     tornado_websocket: 'ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:9432'
