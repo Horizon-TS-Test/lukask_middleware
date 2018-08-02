b525222c1da8d9898292c49eef4c5af736751c7c
José Luis Buenaño Carrillo <37373548+JoseBuenano@users.noreply.github.com>
2018-08-01 22:50:58 -0500

    Update rest-api-url.js

--- src/lukask_middleware/config/rest-api-url.js
+++ src/lukask_middleware/config/rest-api-url.js
@@ -4,14 +4,14 @@ module.exports = {
     quejaType: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/typePub/',
     login: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/login/',
     user: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/userProfile/',
- 	getNotif: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/notification_received/',
+    getNotif: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/notification_received/',
     postNotif: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/notification/',
     pay: 'http://ec2-18-228-7-103.sa-east-1.compute.amazonaws.com:3000/pagar',
     card: 'http://ec2-18-228-7-103.sa-east-1.compute.amazonaws.com:3000/card',
-    exitoso:'http://ec2-18-231-94-189.sa-east-1.compute.amazonaws.com:3001/payment/exitoso',
+    exitoso:'https://ec2-18-231-94-189.sa-east-1.compute.amazonaws.com:3001/payment/exitoso',
     checkout:'http://ec2-18-228-7-103.sa-east-1.compute.amazonaws.com:3000',
     cancelado:'http://ec2-18-228-7-103.sa-east-1.compute.amazonaws.com:3000/cancelado',
-	province: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/province/',
+    province: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/province/',
     canton: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/canton/',
     parroquia: 'http://ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080/lukask-api/parish/'
 }
