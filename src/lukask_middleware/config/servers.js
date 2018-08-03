d919b853245cdee3af90d2c66a30e558019f5fc7
José Luis Buenaño Carrillo <37373548+JoseBuenano@users.noreply.github.com>
2018-08-03 11:17:20 -0500

    Update servers.js

--- src/lukask_middleware/config/servers.js
+++ src/lukask_middleware/config/servers.js
@@ -1,6 +1,6 @@
 module.exports = {
-    allow_origin: 'https://www.lukask.horizon-ts.com',
+    allow_origin: 'http://127.0.0.1:4200',
     //allow_origin: 'http://192.168.1.6:4200',
-    backend_websocket: 'ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:8080',
+    backend_websocket: 'ec2-52-67-33-12.sa-east-1.compute.amazonaws.com:8080',
     tornado_websocket: 'ec2-52-67-204-187.sa-east-1.compute.amazonaws.com:9432'
 }
