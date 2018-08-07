2726062c1b9f3fad417f613bb66a6593e5a71712
Dennys Ivan Moyón Gunsha <36491360+dennysmoyon@users.noreply.github.com>
2018-08-07 17:06:37 -0500

    Update rest-api-url.js

--- src/lukask_middleware/config/rest-api-url.js
+++ src/lukask_middleware/config/rest-api-url.js
@@ -1,17 +1,17 @@
 module.exports = {
-    pub: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/publication/',
-    action: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/actionPub/',
-    quejaType: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/typePub/',
-    login: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/login/',
-    user: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/userProfile/',
-    getNotif: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/notification_received/',
-    postNotif: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/notification/',
+    pub: 'http://54.233.211.204:8080/lukask-api/publication/',
+    action: 'http://54.233.211.204:8080/lukask-api/actionPub/',
+    quejaType: 'http://54.233.211.204:8080/lukask-api/typePub/',
+    login: 'http://54.233.211.204:8080/lukask-api/login/',
+    user: 'http://54.233.211.204:8080/lukask-api/userProfile/',
+    getNotif: 'http://54.233.211.204:8080/lukask-api/notification_received/',
+    postNotif: 'http://54.233.211.204:8080/lukask-api/notification/',
     pay: 'http://ec2-18-228-7-66.sa-east-1.compute.amazonaws.com:3000/pagar',
     card: 'http://ec2-18-228-7-66.sa-east-1.compute.amazonaws.com:3000/card',
     exitoso:'http://ec2-54-233-238-187.sa-east-1.compute.amazonaws.com:3001/payment/exitoso',
     checkout:'http://ec2-18-228-7-66.sa-east-1.compute.amazonaws.com:3000',
     cancelado:'http://ec2-54-233-238-187.sa-east-1.compute.amazonaws.com:3001/payment/cancelado',
-    province: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/province/',
-    canton: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/canton/',
-    parroquia: 'http://ec2-54-207-84-117.sa-east-1.compute.amazonaws.com:8080/lukask-api/parish/'
+    province: 'http://54.233.211.204:8080/lukask-api/province/',
+    canton: 'http://54.233.211.204:8080/lukask-api/canton/',
+    parroquia: 'http://54.233.211.204:8080/lukask-api/parish/'
 }
