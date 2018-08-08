b37ecb8868f107b8d1607baf0d4f779fc7548a92
Dennys Ivan Moyón Gunsha <36491360+dennysmoyon@users.noreply.github.com>
2018-08-08 00:10:47 -0500

    Update servers.js

--- src/lukask_middleware/config/servers.js
+++ src/lukask_middleware/config/servers.js
@@ -1,5 +1,5 @@
 module.exports = {
-    allow_origin: 'https://www.lukask.horizon-ts.com',
-    //allow_origin:'http://127.0.0.1:4200',
+    //allow_origin: 'https://www.lukask.horizon-ts.com',
+    allow_origin:'http://127.0.0.1:4200',
     backend_websocket: '54.233.211.204:8080',
 }
