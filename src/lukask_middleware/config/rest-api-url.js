b52193fe9e8c33f8d8a75d326a7e9bf0cd50a04a
Dennys Ivan Moyón Gunsha <36491360+dennysmoyon@users.noreply.github.com>
2018-08-08 03:29:08 -0500

    Update rest-api-url.js

--- src/lukask_middleware/config/rest-api-url.js
+++ src/lukask_middleware/config/rest-api-url.js
@@ -8,9 +8,9 @@ module.exports = {
     postNotif: 'http://54.233.211.204:8080/lukask-api/notification/',
     pay: 'http://ec2-18-231-59-168.sa-east-1.compute.amazonaws.com:3000/pagar',
     card: 'http://ec2-18-231-59-168.sa-east-1.compute.amazonaws.com:3000/card',
-    exitoso:'https://midredis.lukaksarticles.com/payment/exitoso',
+    exitoso:'https://www.lukaksarticles.com/payment/exitoso',
     checkout:'http://ec2-18-231-59-168.sa-east-1.compute.amazonaws.com:3000',
-    cancelado:'https://midredis.lukaksarticles.com/payment/cancelado',
+    cancelado:'https://www.lukaksarticles.com/payment/cancelado',
     province: 'http://54.233.211.204:8080/lukask-api/province/',
     canton: 'http://54.233.211.204:8080/lukask-api/canton/',
     parroquia: 'http://54.233.211.204:8080/lukask-api/parish/'
