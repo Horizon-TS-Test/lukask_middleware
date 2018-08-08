df479a29af7529ddbc8a6858a469960f4e6e638e
Dennys Ivan Moyón Gunsha <36491360+dennysmoyon@users.noreply.github.com>
2018-08-08 01:48:29 -0500

    Update push-api.js

--- src/lukask_middleware/config/push-api.js
+++ src/lukask_middleware/config/push-api.js
@@ -1,3 +1,3 @@
 module.exports = {
-    webpush_url: 'http://ec2-18-231-59-168.sa-east-1.compute.amazonaws.com:3004/notification'
+    webpush_url: 'http://ec2-18-231-59-168.sa-east-1.compute.amazonaws.com:3003/notification'
 }
