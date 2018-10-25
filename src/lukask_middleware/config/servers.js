d869b3505c44d408672f076aee3f6977a2370732
Darío Vargas <dvargas@horizon-ts.com>
2018-10-25 15:34:17 -0500

    Update servers.js

--- src/lukask_middleware/config/servers.js
+++ src/lukask_middleware/config/servers.js
@@ -1,6 +1,6 @@
 module.exports = {
-    //allow_origin: 'https://www.lukask.horizon-ts.com',
+    allow_origin: 'https://www.lukask.horizon-ts.com',
     //allow_origin:'http://127.0.0.1:4200',
-    allow_origin:'https://horizon-ts.com',
+    //allow_origin:'https://horizon-ts.com',
     backend_websocket: '54.233.211.204:8080',
 }
