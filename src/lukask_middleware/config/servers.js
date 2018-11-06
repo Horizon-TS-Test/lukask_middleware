e2a1a016625e66f1e1e87c9c992ad7995011a65d
fredrom <wfrediromans@outlook.com>
2018-11-06 18:52:12 -0500

    -SETTING UP A SERVERS FILE TO EASY HANDLE

--- src/lukask_middleware/config/servers.js
+++ src/lukask_middleware/config/servers.js
@@ -1,5 +1,7 @@
 module.exports = {
     allow_origin: 'http://127.0.0.1:4200',
-    //allow_origin: 'http://192.168.1.6:4200',
-    backend_websocket: '192.168.1.56:8081',
+    self: 'http://192.168.1.37:3001',
+    backend: 'http://192.168.1.58:8081',
+    backend_websocket: '192.168.1.58:8081',
+    payment: 'http://192.168.1.22:3000'
 }
