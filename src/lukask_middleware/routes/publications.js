d1e70777349ee2d7b5f5282419826408d8eb3769
DARIO VARGAS <dvargas@horizon-ts.com>
2018-11-06 12:27:29 -0500

    Agregacion metodo publicar queja.

--- src/lukask_middleware/routes/publications.js
+++ src/lukask_middleware/routes/publications.js
@@ -98,13 +98,14 @@ router.post('/', upload.array('media_files[]', 5), (req, res, next) => {
   let token = req.session.key.token;
   let dest, mediaArray = [];
 
-  
   if (req.files && req.files.length > 0) {
     //REF: https://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express
     //serverUrl = req.protocol + '://' + req.get('host');
+
     for (var i = 0; i < req.files.length; i++) {
       dest = req.files[i].path;
       dest = dest.replace("public/", "");
+
       mediaArray[mediaArray.length] = {
         mediaType: req.files[i].mimetype.indexOf("image") != -1 ? "IG" : "FL",
         mediaPath: "/" + dest,
@@ -119,7 +120,7 @@ router.post('/', upload.array('media_files[]', 5), (req, res, next) => {
       mediaName: "default.jpg"
     };
   }
-  
+
   var latitud = req.body.latitude;
   var longitud = req.body.longitude;
 
@@ -129,35 +130,83 @@ router.post('/', upload.array('media_files[]', 5), (req, res, next) => {
     apiKey: 'AIzaSyDIjRFZ0hnXtYFoK1uvIHnvQIKoQwkzgUU',
     formatter: null
   };
-  console.log("Entre!!!");
+
   var geocoder = NodeGeocoder(options);
-  
   var Local = {lat: latitud,lon: longitud};
   var location = "";
-  
   let obteniendoCiudad = new Promise((resolve, reject) => {
     geocoder.reverse(Local, (err, res) => {
       if (err) {
         console.log("Error" + err);
       }
       location = res[0].city;
-      console.log("Ciudad");
-      console.log(location);
       resolve("exito");
     });
   });
 
   obteniendoCiudad.then((successMessage) => {
-    console.log("Vamos a publicar!!");
     publicationRestClient.getPubFilter(token, location, function (responseCode, data) {
       if (responseCode == 200) {
         var lista = data.results;
         var respuesta = pv.determinePosition(location, lista, latitud, longitud, token);
         //Verdadero si se puede crear, Falso no se puede crear el registro
-        console.log("respuesta");
-        console.log(respuesta);
         if (respuesta) {
-          console.log("Se inserto...");
+          publicationRestClient.postPub(req.body, mediaArray, token, (responseCode, data) => {
+            if (responseCode == 201) {
+              /*let title = 'Nueva publicación registrada';
+              let content = (req.body.detail.length > 100) ? req.body.detail.substring(0, 100) : req.body.detail;
+              let defaultUrl = '/';
+              let queryParam = data.id_publication;
+              let actions = [
+                {
+                  action: '/?pubId=' + queryParam,
+                  title: 'Ver Pubswall'
+                },
+                {
+                  action: '/mapview?pubId=' + queryParam,
+                  title: 'Ver Mapa'
+                },
+              ]
+              wepushClient.notify(title, content, defaultUrl, actions, function (resCode, notifData) {
+                console.log(resCode, notifData);
+              });*/
+
+              var msg = {
+                stream: "publication",
+                payload: {
+                  action: "custom_create",
+                  pk: data.id_publication,
+                  data: {
+                  }
+                }
+              }
+              ws.send(JSON.stringify(msg));
+
+              ws.onmessage = function (message) {
+                // SOCKET.IO CLIENTS LOGGED CONTROL:
+                //REF: https://stackoverflow.com/questions/35249770/how-to-get-all-the-sockets-connected-to-socket-io
+                Object.keys(req.io.sockets.sockets).forEach(function (id) {
+                  console.log("FROM PUB: Sending data to socket with ID: ", id)  // socketId
+                  if (req.io.sockets.connected[id].request.session.key) {
+                    //REF: https://stackoverflow.com/questions/8281382/socket-send-outside-of-io-sockets-on
+                    req.io.sockets.connected[id].emit("backend-rules", JSON.parse(message.data));
+                  }
+                })
+                ////
+              }
+
+              return res.status(responseCode).json({
+                code: responseCode,
+                title: "Publication has been created successfully",
+                data: data
+              });
+            }
+            return res.status(responseCode).json({
+              code: responseCode,
+              title: "An error has occurred",
+              error: data
+            });
+          });
           } else {
           console.log("No se puede false" + respuesta);
           return res.status(responseCode).json({
@@ -171,7 +220,6 @@ router.post('/', upload.array('media_files[]', 5), (req, res, next) => {
     });
   });
 });
-
 
 
 
