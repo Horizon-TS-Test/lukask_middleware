caf0420c42918066b2d9f5861e6d771bb0711cc9
fredrom <wfrediromans@outlook.com>
2018-11-07 17:32:00 -0500

    -DELETING UNUSED CODE

--- src/lukask_middleware/routes/publications.js
+++ src/lukask_middleware/routes/publications.js
@@ -121,24 +121,6 @@ router.post('/', upload.array('media_files[]', 5), (req, res, next) => {
 
   publicationRestClient.postPub(req.body, mediaArray, token, (responseCode, data) => {
     if (responseCode == 201) {
-      /*let title = 'Nueva publicación registrada';
-      let content = (req.body.detail.length > 100) ? req.body.detail.substring(0, 100) : req.body.detail;
-      let defaultUrl = '/';
-      let queryParam = data.id_publication;
-      let actions = [
-        {
-          action: '/?pubId=' + queryParam,
-          title: 'Ver Pubswall'
-        },
-        {
-          action: '/mapview?pubId=' + queryParam,
-          title: 'Ver Mapa'
-        },
-      ]
-      wepushClient.notify(title, content, defaultUrl, actions, function (resCode, notifData) {
-        console.log(resCode, notifData);
-      });*/
-
       var msg = {
         stream: "publication",
         payload: {
@@ -151,7 +133,7 @@ router.post('/', upload.array('media_files[]', 5), (req, res, next) => {
       ws.send(JSON.stringify(msg));
 
       ws.onmessage = function (message) {
-        // SOCKET.IO CLIENTS LOGGED CONTROL:
+        // SOCKET.IO LOGGED CLIENTS CONTROL:
         //REF: https://stackoverflow.com/questions/35249770/how-to-get-all-the-sockets-connected-to-socket-io
         Object.keys(req.io.sockets.sockets).forEach(function (id) {
           console.log("FROM PUB: Sending data to socket with ID: ", id)  // socketId
